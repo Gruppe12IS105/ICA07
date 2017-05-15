e5e7f4919357af3171cccf6df8839e11a9ec6b7f
Konflux <marign16@student.uia.no>
2017-05-15 13:18:50 +0200

    Oppdatert tcp

--- Ukryptert/TCP/client.go
+++ Ukryptert/TCP/client.go
@@ -1,34 +1,23 @@
-
 package main
 import (
 	"fmt"
 	"net"
-	"./Crypt"
 	"bufio"
 )
 
-var key = Crypt.Randomkey()
-
 func main() {
 	p :=  make([]byte, 2048)
-
-	conn, err := net.Dial("udp", "158.37.63.180:8009")
+	conn, err := net.Dial("tcp", "158.37.63.180:8009")
 	if err != nil {
 		fmt.Printf("Some error %v", err)
 		return
 	}
-
-	var msg, _ = Crypt.AesEncrypt([]byte("Møte fr 5.5 14:45 Flåklypa"), []byte(key))
-	fmt.Fprintf(conn, string(key))
-
-	fmt.Fprintf(conn, string(msg))
-
+	fmt.Fprintf(conn, "Møte Fr 5.5 14:45 Flåklypa")
 	_, err = bufio.NewReader(conn).Read(p)
 	if err == nil {
 		fmt.Printf("%s\n", p)
 	} else {
 		fmt.Printf("Some error %v\n", err)
 	}
-
 	conn.Close()
 }
---
+++ Ukryptert/TCP/server.go
@@ -0,0 +1,64 @@
+
+package main
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+
+func sendResponse(conn *net.UDPConn, addr *net.UDPAddr) {
+	_,err := conn.WriteToUDP([]byte("From server: Hello I got your mesage "), addr)
+	if err != nil {
+		fmt.Printf("Couldn't send response %v", err)
+	}
+}
+
+
+func main() {
+	p := make([]byte, 2048)
+	addr := net.TCPAddr{
+		Port: 8009,
+		IP:   net.ParseIP("0.0.0.0"),
+	}
+	ser, err := net.ListenTCP("tcp", &addr)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+		return
+	}
+	for {
+		conn, err := ser.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+
+		// Close the listener when the application closes.
+		defer ser.Close()
+		for {
+			// Listen for an incoming connection.
+			conn, err := ser.Accept()
+			if err != nil {
+				fmt.Println("Error accepting: ", err.Error())
+				os.Exit(1)
+			}
+			// Handle connections in a new goroutine.
+			go handleRequest(conn)
+		}
+	}
+}
+
+	// Handles incoming requests.
+func handleRequest (conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	// Read the incoming connection into the buffer.
+	reqLen, err := conn.Read(buf)
+	if err != nil {
+	fmt.Println("Error reading:", err.Error())
+	}
+	// Send a response back to person contacting us.
+	conn.Write([]byte("Message received."))
+	// Close the connection when you're done with it.
+	conn.Close()
+	}
