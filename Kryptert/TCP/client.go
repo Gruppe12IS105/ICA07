35383ced980f1de0ac40f03d4d884567e044d986
Konflux <marign16@student.uia.no>
2017-05-18 20:41:52 +0200

    Updates

--- Kryptert/TCP/client.go
+++ Kryptert/TCP/client.go
@@ -10,7 +10,7 @@ import (
 var key = []byte("TestkeyHei")
 
 func main() {
-	str := "Møte fr 5.5 14:45 Flåklypa"
+	str := "Mote fr 5.5 14:45 Flåklypa"
 	servAddr := "158.37.63.180:8009"
 		tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
 		if err != nil {
@@ -29,8 +29,10 @@ func main() {
 	reply := make([]byte, 1024)
 	conn.Read(reply)
 	msg, _ := Crypt.AesDecrypt([]byte(reply), key)
+
 	//fmt.Println("reply from server=", (reply))
 	//fmt.Printf("%s", msg)
+
 	fmt.Printf ("Your reply is")
 	fmt.Printf("%s", msg)
 	conn.Close()
